6ecb6af736979db6fc83124b513e003b680b2c99
Purpel Lizrd <vladislav.nekrasov123@gmail.com>
2023-06-06 20:30:05 +0300

    SpecialAbility abstract class implementation.

---
+++ StackMortalKombat/Interfaces/SpecialAbility.cs
@@ -0,0 +1,46 @@
+﻿namespace StackMortalKombat.Interfaces;
+
+public abstract class SpecialAbility : IUnit
+{
+    #region SpecialAbility related
+    protected IUnit _unit;
+    public int SpecialAbilityRange { get; }
+    public int SpecialAbilityStrength { get; }
+    public string SpecialAbilityName { get; }
+    public uint SpecialAbilityCost { get; }
+    #endregion
+
+    #region IUnit related
+    public uint Id => _unit.Id;
+    public string Name => SpecialAbilityName + " " + _unit.Name;
+    public int Health => _unit.Health;
+    public int MaxHP => _unit.MaxHP;
+    public uint Damage => _unit.Damage;
+    public uint Defense => _unit.Defense;
+    public uint AttackRange => _unit.AttackRange;
+    public uint Cost => SpecialAbilityCost + _unit.Cost;
+    #endregion
+
+
+    public SpecialAbility(IUnit unit, int specialAbilityRange, int specialAbilityStrength, string specialAbilityName, uint specialAbilityCost)
+    {
+        _unit = unit;
+        SpecialAbilityRange = specialAbilityRange;
+        SpecialAbilityStrength = specialAbilityStrength;
+        SpecialAbilityName = specialAbilityName;
+        SpecialAbilityCost = specialAbilityCost;
+    }
+
+
+    public abstract void CastSpecialAbility(ref List<IUnit> unitsFriendly, ref List<IUnit> unitsEnemies);
+
+    public virtual void DamageTaken(uint damageTaken)
+    {
+        _unit.DamageTaken(damageTaken);
+    }
+
+    public virtual void TakeTurn()
+    {
+        _unit.TakeTurn();
+    }
+}
--- StackMortalKombat/Program.cs
+++ StackMortalKombat/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int n = -3;
+Console.WriteLine(Convert.ToUInt32(n));
+
+
