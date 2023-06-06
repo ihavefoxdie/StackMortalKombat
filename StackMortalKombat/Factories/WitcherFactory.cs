9d42a69652a470ff7af29bc2086e8c43933408c2
Gusiha <TUGARIN@TUGARIN>
2023-06-06 20:50:12 +0300

    temp

--- StackMortalKombat/Factories/WitcherFactory.cs
+++ StackMortalKombat/Factories/WitcherFactory.cs
@@ -1,18 +1,20 @@
 ﻿using StackMortalKombat.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StackMortalKombat.Factories
 {
     internal class WitcherFactory : UnitFactory
     {
+        private IUnit _unit;
+        public WitcherFactory(IUnit unit)
+        {
+            _unit = unit;
+        }
+
         public override IUnit CreateUnit()
         {
-            InfantryFactory infantryFactory = new InfantryFactory();
-            IUnit unit = infantryFactory.CreateUnit();
+            
+
+
 
             throw new NotImplementedException();
 
